658bc51dd9162c4907586ceb833979ddd7e8ec35
quocthang28 <quocthang28@github.com>
2020-07-30 20:38:31 +0700

    .

--- QLKS/QLKS/frmBaoCao.cs
+++ QLKS/QLKS/frmBaoCao.cs
@@ -23,39 +23,36 @@ namespace QLKS
             String a = cbMonth.Text;
             using (SqlConnection conn = new SqlConnection(@"Data Source=.;Initial Catalog=QLKS;Integrated Security=True;MultipleActiveResultSets=True"))
             {
-                //try
-                //{
+                try
+                {
                     conn.Open();
                     string query = string.Format("select tenloaiphong as 'Tên loại phòng',doanhthu as 'Doanh thu theo tháng',D.maloaiphong as 'Tỷ Lệ' from (SELECT maloaiphong, sum(thanhtien) as DoanhThu from(select P.maloaiphong, thanhtien, P.maphong from(select C.maphong, C.thanhtien from hoadon H, CHITIETHOADON C where H.mahoadon = C.mahoadon AND month(H.NGAYLAP) = '{0}') as A, PHONG P WHERE A.maphong = P.MAPHONG) as B Group by maloaiphong) as D, LOAIPHONG M WHERE D.maloaiphong = M.MALOAIPHONG", a);
 
                     string query2 = "SELECT sum(thanhtien) as DoanhThu from (select P.maloaiphong, thanhtien, P.maphong from(select C.maphong, C.thanhtien from hoadon H, CHITIETHOADON C where H.mahoadon = C.mahoadon AND year(H.NGAYLAP) = '2020') as A, PHONG P WHERE A.maphong = P.MAPHONG) as B";
                     SqlCommand cmd2 = new SqlCommand(query2, conn);
-                    //Tổng doanh thu một năm
                     float totalMoney = (float)Convert.ToInt32(cmd2.ExecuteScalar().ToString());
                     SqlCommand cmd = new SqlCommand(query, conn);
                     SqlDataAdapter dAdapter = new SqlDataAdapter(cmd);
                     DataTable dt = new DataTable();
                     dAdapter.Fill(dt);
-                   
+
                     foreach (DataRow currentRow in dt.Rows)
                     {
-                        //row["Tỷ Lệ"] =
                         currentRow.BeginEdit();
-                    var temp = Math.Round(Convert.ToDecimal(100 * (float)Convert.ToInt32(currentRow["Doanh thu theo tháng"].ToString()) / totalMoney), 2);
-
-                    currentRow["Tỷ Lệ"] = (float)temp + " %";
-                    currentRow.EndEdit();
+                        var temp = Math.Round(Convert.ToDecimal(100 * (float)Convert.ToInt32(currentRow["Doanh thu theo tháng"].ToString()) / totalMoney), 2);
+                        currentRow["Tỷ Lệ"] = (float)temp + " %";
+                        currentRow.EndEdit();
                     }
 
                     dgvDoanhThu.DataSource = dt;
-                //}
-                //catch (Exception)
-                //{
-                //}
-                //finally
-                //{
-                //    conn.Close();
-                //}
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    conn.Close();
+                }
             }
         }
     }
